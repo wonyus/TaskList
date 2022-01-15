8707cc750ad1aa28bea52c0e84be2f519d0abc89
WonYus <lucy_oliv@outlook.com>
2022-01-15 14:15:15 +0700

    bg color  card color

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -7,7 +7,7 @@
 
     <div class="py-12">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-xl sm:rounded-lg p-5">
+            <div class="bg-[#f9faec] overflow-hidden shadow-xl sm:rounded-lg p-5">
             
                 <form method="POST" action="/task">
 
