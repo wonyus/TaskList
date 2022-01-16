2e68775d611cf3f704dfbc6ddb911bac9132c582
WonYus <lucy_oliv@outlook.com>
2022-01-16 21:48:48 +0700

    Update add.blade.php

--- resources/views/add.blade.php
+++ resources/views/add.blade.php
@@ -5,81 +5,67 @@
         </h2>
     </x-slot>
     <style>
-            body {
-                font-family: 'Nunito';
-            }
-            
-            #video_wrapper
-            {
-            margin:0px;
-            padding:0px;
-            }
-            #myVideo {
-            position: none;
-            right: 0;
-            bottom: 0;
-            min-width: 50%; 
-            min-height: 50%;
-            font-size:40px;
-            
-            }
-            .Bg{
-                padding: 50px;
-                width: 150px;
-                position: fixed ;
-                bottom: 40%;
-                left: 26%;
-                width :45%;
-            }
-            img{
-                border-radius: 50%;
-                margin-left: 20px;
-                width:50px;
-                height:50px;
-            }
-            .h1{
-                font-family: Fantasy;
-                font-size: 50px;
-                color: #4682B4;
-            }
-            .task{
-                font-family: Fantasy;
-                font-size: 23px; 
-                color: #FFFACD; 
-            }
-            .line{
-                display: flex; 
-            }
-        </style>
+        body {
+            font-family: 'Nunito';
+        }
+
+        .Bg {
+            width: 750px;
+            text-align: center;
+            margin: auto;
+        }
+
+        img {
+            border-radius: 50%;
+            margin-left: 20px;
+            width: 50px;
+            height: 50px;
+        }
+
+        .h1 {
+            font-family: Fantasy;
+            font-size: 50px;
+            color: #4682B4;
+        }
+
+        .task {
+            font-family: Fantasy;
+            font-size: 23px;
+            color: #FFFACD;
+        }
+
+        .line {
+            display: flex;
+        }
+    </style>
+
     <body class="antialiased">
-        <video autoplay muted loop id="myVideo">
-        <source src="beach.mp4" type="video/mp4">
-        </video>
+
         <div class="Bg">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
+            <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
 
 
-            <div class="bg-clip-border p-6 bg-[#f9faec] border-4 border-sky-600 border-dashed overflow-hidden shadow-xl sm:rounded-lg p-5">
-           
-                <form method="POST" action="/task">
-                    
-                    <div class="form-group">
-                        <textarea name="description" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-font-style: italic placeholder-gray-700 focus:outline-none focus:bg-white"  placeholder='Enter your task'></textarea>  
-                        @if ($errors->has('description'))
-                        <span class="text-danger font-style: italic font-medium ">{{ $errors->first('description') }}</span>
-                        @endif
-                    </div>
-                    
-                    
+                <div class="bg-clip-border p-6 bg-[#f9faec] border-4 border-sky-600 border-dashed overflow-hidden shadow-xl sm:rounded-lg p-5">
 
-                    <div class="form-group">
-                        <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-style: italic font-bold py-2 px-4 rounded">Add Task</button>
-                    </div>
-                    {{ csrf_field() }}
-                </form>
+                    <form method="POST" action="/task">
+
+                        <div class="form-group">
+                            <textarea name="description" class="bg-gray-100 rounded border border-gray-400 leading-normal resize-none w-full h-20 py-2 px-3 font-font-style: italic placeholder-gray-700 focus:outline-none focus:bg-white" placeholder='Enter your task'></textarea>
+                            @if ($errors->has('description'))
+                            <span class="text-danger font-style: italic font-medium ">{{ $errors->first('description') }}</span>
+                            @endif
+                        </div>
+
+
+
+                        <div class="form-group">
+                            <button type="submit" class="bg-blue-500 hover:bg-blue-700 text-white font-style: italic font-bold py-2 px-4 rounded">Add Task</button>
+                        </div>
+                        {{ csrf_field() }}
+                    </form>
+                </div>
             </div>
         </div>
-    </div>
 
-        </body>
+    </body>
 </x-app-layout>
